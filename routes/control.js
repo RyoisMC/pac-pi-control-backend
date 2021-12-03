a67540d9f18192f174e55f4696c9ae0a85dd64c2
Ryois <ryois@ryois.me>
2021-12-03 14:00:43 -0500

    Add in more APIs

--- routes/control.js
+++ routes/control.js
@@ -3,5 +3,7 @@ const router = new Router();
 
 const ctrl_controlller = require('../controllers/control');
 
+router.get('/api/control/status', ctrl_controlller.get_cstatus);
 router.post('/api/control/mode', ctrl_controlller.set_mode);
+router.post('/api/control/power', ctrl_controlller.power);
 module.exports = router;
