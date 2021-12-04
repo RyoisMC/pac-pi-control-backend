6b33ac8ab3f0162ac4fb34ea76600a5c1da10aee
Ryois <ryois@ryois.me>
2021-12-04 17:44:43 -0500

    Add in heartbeat route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const x32 = require('./x32');
 const control = require('./control');
 const apiResponse = require('../services/apiResponse');
-
+const config = require('../private/config.json');
 
 module.exports = app => {
 	app.use(x32);
     app.use(control);
+	app.get('/api/heartbeat', function(req, res) {
+		res.status(200);
+		return res.json(apiResponse({
+			error: false,
+			data: { 'display_ip': req.headers['x-forwarded-for'] || req.socket.remoteAddress, 'x32_ip': config.X32_IP}
+		}));
+	});
 	app.get('*', function(req, res) {
 		res.status(404);
 		return res.json(apiResponse({
